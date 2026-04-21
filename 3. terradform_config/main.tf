55c1122dc89183fb7d7bb4cfc449795df257e514
fuyiran111 <fuyiran111@hotmail.com>
2026-04-21 15:11:05 +0800

    first commit

---
+++ 3. terradform_config/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-east-1"
+}
+
+resource "aws_instance" "terraform-created1" {
+  ami           = "ami-0a3e33e468c258981"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Hello-world"
+  }
+}
